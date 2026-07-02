d3d5d097f7282ca5035e79e236da98f22d0ab5ea
Alex Andriopoulos <alandriopoulos@entersoftone.gr>
2026-07-02 12:43:31 +0300

    added account model and service

---
+++ save our souls/Models/UserAccount.cs
@@ -0,0 +1,19 @@
+﻿using SQLite;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace save_our_souls.Models
+{
+    class UserAccount
+    {
+        [PrimaryKey, AutoIncrement]
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public string Username { get; set; }
+
+        public string Password { get; set; }
+    }
+}
---
+++ save our souls/Services/UserAccountDatabase.cs
@@ -0,0 +1,80 @@
+﻿using SQLite;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Security.Cryptography;
+
+namespace save_our_souls.Services
+{
+    class UserAccountDatabase
+    {
+        private const int SaltSize = 16;      // 128-bit
+        private const int HashSize = 32;      // 256-bit
+        private const int Iterations = 100_000;
+
+        SQLiteAsyncConnection? db;
+
+        async Task Init()
+        {
+            if (db != null)
+                return;
+            db = new SQLiteAsyncConnection(Constants.DatabasePath, Constants.Flags);
+            var result = await db.CreateTableAsync<Models.UserAccount>();
+        }
+
+        public async Task AddUserAccountAsync(Models.UserAccount userAccount)
+        {
+            await Init();
+            userAccount.Password = HashPassword(userAccount.Password);
+            await db.InsertAsync(userAccount);
+        }
+
+        public async Task<Models.UserAccount?> GetUserAccountAsync(string username, string password)
+        {
+            await Init();
+            var userAccount = await db.Table<Models.UserAccount>()
+                .Where(u => u.Username == username)
+                .FirstOrDefaultAsync();
+
+            if (userAccount == null)
+                return null;
+
+            if (VerifyPassword(password, userAccount.Password))
+                return userAccount;
+
+            return null;
+        }
+
+        private static string HashPassword(string password)
+        {
+            byte[] salt = new byte[SaltSize];
+            RandomNumberGenerator.Fill(salt);
+
+            byte[] hash = Rfc2898DeriveBytes.Pbkdf2(
+                password,
+                salt,
+                Iterations,
+                HashAlgorithmName.SHA256,
+                HashSize);
+
+            return $"{Iterations}.{Convert.ToBase64String(salt)}.{Convert.ToBase64String(hash)}";
+        }
+
+        public static bool VerifyPassword(string password, string hashedPassword)
+        {
+            var parts = hashedPassword.Split('.');
+            if (parts.Length != 3)
+                throw new FormatException("Unexpected hash format. Should be formatted as '{iterations}.{salt}.{hash}'");
+            int iterations = Convert.ToInt32(parts[0]);
+            byte[] salt = Convert.FromBase64String(parts[1]);
+            byte[] hash = Convert.FromBase64String(parts[2]);
+            byte[] testHash = Rfc2898DeriveBytes.Pbkdf2(
+                password,
+                salt,
+                iterations,
+                HashAlgorithmName.SHA256,
+                hash.Length);
+            return CryptographicOperations.FixedTimeEquals(testHash, hash);
+        }
+    }
+}
